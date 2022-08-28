0fb8c88f476fa33946bafbdbc06425ca1d18a31a
violet666666 <setanhantu11111@gmail.com>
2022-08-28 15:58:31 +0700

    add increment decrement

---
+++ increment decrement.js
@@ -0,0 +1,8 @@
+/* Increment dan Decrement */
+
+let postfix = 5;
+console.log(postfix++); /* Output: 5 */
+console.log(postfix); /* Output: 6 */
+
+let prefix = 5;
+console.log(++prefix); /* Output: 6 */
