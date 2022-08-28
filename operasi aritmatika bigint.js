7449677029788b84283d75a9facf905c118fe91e
violet666666 <setanhantu11111@gmail.com>
2022-08-28 16:56:10 +0700

    add boolean, null, aritmatika bigint, and string

---
+++ operasi aritmatika bigint.js
@@ -0,0 +1,14 @@
+console.log(5 n + 2 n);
+console.log(5 n - 2 n);
+console.log(5 n * 2 n);
+console.log(5 n / 2 n);
+console.log(5 n % 2 n);
+
+/* Output:
+7n
+3n
+10n
+2n (Bukan 2.5n)
+1n
+dengan menggunakan bigint maka hasil pembagiannya akan dibulatkan kebawah
+*/
---
+++ string.js
@@ -0,0 +1,11 @@
+let greet = 'Hello';
+console.log(typeof(greet));
+
+/* Output: string 
+Tidak ada perbedaan antara menggunakan petik satu atau petik dua. Anda dapat menggunakan tanda petik secara bergantian, 
+khususnya jika Anda memiliki teks yang mengandung tanda petik.
+*/
+let moreGreet = greet + " " + greet;
+console.log(moreGreet);
+
+/* Output: Hello Hello */
