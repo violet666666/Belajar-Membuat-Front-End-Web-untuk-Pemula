c4be4ff23c38383b10c83a6ec5ef6a6a7f014544
violet666666 <setanhantu11111@gmail.com>
2022-08-28 19:45:07 +0700

    add to do list for quiz

---
+++ Tipe data/kuis variabel dan tipe data/TO-DO.js
@@ -0,0 +1,24 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = "Tegar";
+let lastName = "Alfalkh Thamrin";
+let age = 20;
+let isMarried = false;
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+    firstName,
+    lastName,
+    age,
+    isMarried,
+};
