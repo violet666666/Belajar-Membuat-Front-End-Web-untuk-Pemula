2fd5916718edffceee2ff8fd210ff5ac88ed5b50
violet666666 <setanhantu11111@gmail.com>
2022-08-30 16:22:27 +0700

    add modul object dan kuis object

---
+++ Object/spaceship delete.js
@@ -0,0 +1,21 @@
+const spaceship = {
+    name: "Millenium Falcon",
+    manufacturer: "Corellian Engineering Corporation",
+    maxSpeed: 1200,
+    color: "Light gray",
+};
+
+spaceship.color = "Glossy red";
+spaceship["maxSpeed"] = 1300;
+
+delete spaceship.manufacturer;
+
+console.log(spaceship);
+
+/* Output:
+{ 
+  name: 'Millenium Falcon', 
+  maxSpeed: 1300, 
+  color: 'Glossy red'
+}
+ */
