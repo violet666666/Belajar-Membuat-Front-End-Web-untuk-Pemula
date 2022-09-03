30d5548e7bcb04286043c998f99fa0838f45889d
violet666666 <setanhantu11111@gmail.com>
2022-09-04 00:06:56 +0700

    add some file bom dom

---
+++ BOM dan DOM/Membuat Elemen HTML/new element.js
@@ -0,0 +1,4 @@
+const newElement = document.createElement("p");
+newElement.innerHTML = "<b>Selamat datang</b> ke HTML kosong ini :)";
+const newElement = document.createElement("img");
+newElement.setAttribute("src", "https://picsum.photos/200/300");
